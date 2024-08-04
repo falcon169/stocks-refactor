28682b131daba039e961ef8ec92b6a665f39a1ca
Lavan Kumar Peechara <lavan.peechara@gmail.com>
2024-08-04 15:00:06 +0000

    text works

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit your wish  <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learning React
-          </a>
-        </p>
-      </header>
-    </div>
+      <div
+      >
+        <h1 className="text-center my-4">Stock Portfolio</h1>
+
+      </div>
   );
 }
 
